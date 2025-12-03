d24872fad3d52fe731f34f945c126f6b6ae98807
jenifferalejandracollazos-droid <jenifferalejandracollazos@gmail.com>
2025-12-03 02:55:45 +0000

    family-terminada

--- src/app.py
+++ src/app.py
@@ -35,8 +35,34 @@ def handle_hello():
     members = jackson_family.get_all_members()
     response_body = {"hello": "world",
                      "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
+                # miembro por id
+@app.route('/members/<int:id>', methods=['GET'])
+def get_single_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"message": "Member not found"}), 404
+
+
+
+
+@app.route('/members', methods=['POST'])
+def handle_post():
+    data = request.get_json()
+    new_member = jackson_family.add_member(data) 
+    return jsonify(new_member), 200
+
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    success = jackson_family.delete_member(id)
+    if success:
+        return jsonify({"done": True}), 200
+    else:
+        return jsonify({"message": "Member not found"}), 404
 
 
 # This only runs if `$ python src/app.py` is executed
